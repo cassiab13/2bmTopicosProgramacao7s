c959d8e15c069498891b9bdd649af4f58ea51497
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-14 21:06:13 -0300

    feat: add enums

--- src/main/java/com/example/tarefas/entities/User.java
+++ src/main/java/com/example/tarefas/entities/User.java
@@ -1,5 +1,6 @@
 package com.example.tarefas.entities;
 
+import com.example.tarefas.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,7 +12,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+
     private String name;
+
     private String email;
+
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
