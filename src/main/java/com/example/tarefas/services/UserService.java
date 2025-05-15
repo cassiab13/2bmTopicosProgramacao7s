b0ef9fd0eeb6231145b19b0491c40517a0933dcd
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-14 21:27:17 -0300

    feat: create repository and service: createUser and findbyid

--- src/main/java/com/example/tarefas/services/UserService.java
+++ src/main/java/com/example/tarefas/services/UserService.java
@@ -1,9 +1,32 @@
 package com.example.tarefas.services;
 
+import com.example.tarefas.entities.Users;
+import com.example.tarefas.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.Users;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    @Autowired
+    UserRepository repository;
 
+    public Users create(Users user){
+        return repository.save(user);
+    }
+
+    public Users findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
